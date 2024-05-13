9845f8fed1a8c5547a58c9c16130098281161852
kotsuya <kotsuya00@gmail.com>
2024-05-13 15:44:21 +0900

    Add env file

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwLg9gOOMqWpH7Lxzzc_C3Af8V-HNBAQs",
-  authDomain: "chat-app-react-firebase-566ec.firebaseapp.com",
-  projectId: "chat-app-react-firebase-566ec",
-  storageBucket: "chat-app-react-firebase-566ec.appspot.com",
-  messagingSenderId: "164587804154",
-  appId: "1:164587804154:web:4a94f93b5585407ea74043"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STOORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
